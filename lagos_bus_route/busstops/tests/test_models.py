143fdcac9a1b28cd9cb99c7469929e89b095181c
andela-cdike <chukwuerika.dike@andela.com>
2016-12-29 22:52:46 +0100

    Setup coverage

--- lagos_bus_route/busstops/tests/test_models.py
+++ lagos_bus_route/busstops/tests/test_models.py
@@ -1,2 +1,14 @@
 from django.test import TestCase
 
+from factories.factories import RouteFactory
+
+
+class RouteModelTestSuite(TestCase):
+    def test_model_string_representation(self):
+        route_node = RouteFactory()
+        exp_output = '{0}, type-{1}, route {2}'.format(
+            route_node.busstop.name,
+            route_node.busstop_type,
+            route_node.route_id
+        )
+        self.assertEqual(str(route_node), exp_output)
