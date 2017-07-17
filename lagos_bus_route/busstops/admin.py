31c00c2bcdf140786e6235470fdc540dec5ef013
andela-cdike <chukwuerika.dike@andela.com>
2017-07-17 23:47:09 +0100

    Add list fields and search to Busstop admin page

--- lagos_bus_route/busstops/admin.py
+++ lagos_bus_route/busstops/admin.py
@@ -3,5 +3,7 @@ from django.contrib import admin
 from busstops.models import BusStop
 
 
-
-admin.site.register(BusStop)
+@admin.register(BusStop)
+class BusStopAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'latitude', 'longitude', 'place_id')
+    search_fields = ('name',)
